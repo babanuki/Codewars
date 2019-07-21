4055f108e667d3784b7fd5a8e35c79fa90eab976
babanuki <52341660+babanuki@users.noreply.github.com>
2019-07-22 07:37:16 +0900

    Update Your order, please.py

--- Your order, please.py
+++ Your order, please.py
@@ -14,3 +14,13 @@ def order(sentence):
         del j[0]
         ansl.append("".join(j))
     return " ".join(ansl)
+
+
+'''
+def order(sentence):
+    return " ".join(sorted(sentence.split(), key=lambda words: sorted(words)))
+    
+    
+    
+sorted()와 sort()는 key function으로 정렬 기준을 정할 수 있음.
+'''
