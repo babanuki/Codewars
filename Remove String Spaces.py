0989d3a4cc1638ef7894c0addbcba4ae51626377
babanuki <52341660+babanuki@users.noreply.github.com>
2019-07-23 03:25:15 +0900

    Create Remove String Spaces.py

---
+++ Remove String Spaces.py
@@ -0,0 +1,13 @@
+def no_space(x):
+    return ''.join([i for i in x if i is not ' '])
+    
+    
+    
+'''
+def no_space(x):
+    return x.replace(' ' ,'')
+    
+    
+def no_space(x):
+    return "".join(x.split())
+'''
