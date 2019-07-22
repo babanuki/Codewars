23ca549d62876d8779b4c2b98b9046cf814f0f09
babanuki <babanuki24@gmail.com>
2019-07-23 05:39:47 +0900

    6 kyu

--- Python3/Unique_In_Order.py
+++ Python3/Unique_In_Order.py
@@ -7,3 +7,27 @@ def unique_in_order(iterable):
         else:
             i+=1
     return l
+
+
+
+'''
+from itertools import groupby
+
+def unique_in_order(iterable):
+    return [k for (k, _) in groupby(iterable)]
+
+
+
+
+unique_in_order = lambda l: [z for i, z in enumerate(l) if i == 0 or l[i - 1] != z]
+
+
+
+
+
+def unique_in_order(iterable):
+  r = []
+  for x in iterable:
+    x in r[-1:] or r.append(x)
+  return r
+  '''
